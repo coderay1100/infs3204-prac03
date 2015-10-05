e6dfe3486a1090f2a57fc316f1451f42220eea5d
coderay1100 <rayp1100@gmail.com>
2015-10-05 12:51:42 +1000

    initial commit

---
+++ infs3204-prac03/IBookStore.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace infs3204_prac03
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBookStore" in both code and config file together.
+    [ServiceContract]
+    public interface IBookStore
+    {
+        [OperationContract]
+        Book[] GetAllBooks();
+    }
+
+    [DataContract]
+    public class Book
+    {
+        [DataMember]
+        public string ID;
+
+        [DataMember]
+        public string name;
+
+        [DataMember]
+        public string author;
+
+        [DataMember]
+        public int year;
+
+        [DataMember]
+        public float price;
+
+        [DataMember]
+        public int stock;
+
+        public Book(string[] data)
+        {
+            ID = data[0];
+            name = data[1];
+            author = data[2];
+            year = int.Parse(data[3]);
+            price = float.Parse(data[4].Substring(1));
+            stock = int.Parse(data[5]);
+        }
+    }
+}
